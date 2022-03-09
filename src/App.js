7e3aa68e2681eb28de98e0b39a4d88cf9f4b2b8d
NusraThawfeek <nusra0408@gmail.com>
2022-03-09 12:25:16 +0530

    done apis for read and write

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    async function fetcher() {
+      const response = await fetch('http://localhost:3001/contacts');
+      const contacts = await response.json();
+
+      setContacts(contacts);
+    }
+
+    fetcher();
+  }, []);
+  console.log(contacts);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div>
+      <ul>
+        {
+          contacts.map(contact => (
+            <li key={contact.id}>
+              <p>Name: {contact.name}</p>
+              <span>Phone: {contact.phone}</span>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
